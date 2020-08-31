9c827eb6759e1a9d5bb58443974880648755cd0d
Felipe Jonas <felipe.gabriel.novais.jo@gmail.com>
2020-08-30 21:42:21 -0300

    done!

--- second_year/review/printTypes.py
+++ second_year/review/printTypes.py
@@ -22,6 +22,7 @@ print('Amanda tem {} anos de idade e {} pets\n'.format(idade,pets))
 # %
 troco = 3.45
 nome = 'Paulo'
+bolso = 5.65
 print('Carlinhos recebeu de %f \n' %troco)
 print('%s recebeu %f de troco \n'%(nome, troco))
 # Agora alterando o número de casas depois da vírgula - '%0.2f' = Sem zero a esquerda(posições) e apenas 2 casas decimais.
@@ -31,5 +32,7 @@ print('O troco é de : %20f \n' %troco)
 # %020f = o conteúdo começara a aparecer com o intervalo de 20 posições (espaços em branco) COM zeros a esquerda;
 print('O troco é de : %020f \n' %troco)
 # ===============================================================================
-
-# 6
+# 6 - Compor string, escohendo a posição (espaços em brancos) para iniciar o print.
+# Assim adiciono mais de 5 posições posições em branco antes da palavra "tem" e reduzo para menor que 10 após a palavra.
+# Depois adiciono 5 espaços em branco antes do valor decimal, e deixo apenas 2 casas decimais.
+print('{:>5}tem {:<10} anos e apenas R$ {:5.2f} no bolso \n'.format(nome, idade, bolso))
