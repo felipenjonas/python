956d7b1dc8854e408fbe115453f345c6dc77c34a
Felipe Jonas <felipe.gabriel.novais.jo@gmail.com>
2020-08-30 21:03:05 -0300

    initial commit

---
+++ second_year/review/lista.py
@@ -0,0 +1,21 @@
+# Programa que cria uma lista (vetor) com N elementos,
+# Insere o valores neste Array (input ou randit) e em seguida,
+# mostrar o MENOR e o maior valor da lista.
+
+numbers_list = [  ]
+
+start = 0
+amount = int(input('Digite um valor: \n'))
+
+for item in range(amount):
+    start += 1 
+    numbers_list.append(start)
+
+
+print(f'Lista:{numbers_list} \n')
+
+print(f'O Valor mais alto da lista: [ {max(numbers_list)} ]\n')
+print(f'O menor valor da lista é: [ {min(numbers_list)} ] \n')
+
+
+
