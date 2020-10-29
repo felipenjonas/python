4efb203d47e3cf877375cb21705ed1e63ca97de1
Felipe Jonas <felipe.gabriel.novais.jo@gmail.com>
2020-10-29 18:42:23 -0300

    add

---
+++ second_year/review/dicionario.py
@@ -0,0 +1,37 @@
+# Sobre estrutura de dados em python
+# Chave: Valor
+# Chaves podem ser string ou numero, e valores tbm
+dicionario = {}
+
+# print(dicionario)
+
+
+pessoa = { 
+    'nome':'Felipe',
+    'idade': 20,
+    'dataNascimento': '15/05/200'
+}
+
+print(pessoa)
+
+#Consultas valores de chaves do dicionário 
+print(pessoa['nome'])
+print(pessoa['idade'])
+print(pessoa['dataNascimento'])
+
+# Adicionar novas chaves e valores dentor do dicionário:
+pessoa['corFavorita'] = 'Preto'
+
+print(pessoa)
+
+# Outros métodos:
+
+# Mostra as chaves, também seus valores
+print(pessoa.keys())
+
+
+# Mostrar os valroes
+print(pessoa.values())
+
+# LIMPAR ou deleta:
+print(pessoa.clear())
